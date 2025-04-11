bb92bfd170a86bb12c29f110ced3aac8ccb70ecd
rionguyenfcs <hau.nguyen@floatingcube.com>
2025-04-11 10:55:53 +0700

    add function download images

--- assets/web/js/components/layouts/ImageDownloader.js
+++ assets/web/js/components/layouts/ImageDownloader.js
@@ -1,10 +1,37 @@
 import { Box, Button } from "@mui/material";
 import React from "react";
+import { useMainProvider } from "../../providers/MainProvider";
+import { AlertStatus, showAlert } from "../../helpers/showAlert";
 
 const ImageDownloader = () => {
+  const { selectedImages } = useMainProvider();
+  
+  const handleDownloadImages = async () => {
+    if (!selectedImages || selectedImages.length <= 0) {
+      showAlert(
+        AlertStatus.warning,
+        "Failed",
+        "Select images that you want to download!"
+      );
+      return;
+    }
+    await downloadPreviewImages(selectedImages);
+  }
+
+  const downloadPreviewImages = async (images) => {
+    await images.map((img, index) => { 
+      const link = document.createElement("a");
+      link.href = img.preview;
+      link.setAttribute("download", img.file.name || `image-${index + 1}.jpg`);
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    });
+  };
+
   return (
     <Box>
-      <Button>Download</Button>
+      <Button onClick={handleDownloadImages}>Download</Button>
     </Box>
   );
 };
