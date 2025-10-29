340a3d6ccec3abd77dba47ed9f7c9db14c033acd
rionguyenfcs <hau.nguyen@floatingcube.com>
2025-10-29 14:08:13 +0700

    update whatsapp button in preview page

--- src/woocommerce/templates/photobook/preview-item.php
+++ src/woocommerce/templates/photobook/preview-item.php
@@ -18,5 +18,12 @@ $accepted = $item->get_meta('photobook_template_accepted', false);
                 <button class="p-btn-primary accept-btn">Accept this version</button>
             </div>
         <?php endif; ?>
+        <div class="contact-via-whatsapp" style="margin-top: 20px;">
+            <h6 style="margin-bottom: 10px;">*Need to contact us? Chat with us below.</h6>
+            <a href="https://wa.me/+6596959753" target="_blank" type="button" class="whatsapp-btn">
+                <img width="25px" src="<?php echo plugin_dir_url('zippy-photos/assets/icons/whatsapp.svg') . 'whatsapp.svg' ?>" alt="whatsapp:">
+                Whatsapp
+            </a>
+        </div>
     </div>
 </div>
